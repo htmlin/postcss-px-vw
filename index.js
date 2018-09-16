c0ff69e2f8237899b88de0c2b7c496358d820b9e
hardylinfee <10543563@qq.com>
2018-09-16 21:25:11 +0800

    fix bug

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ module.exports = postcss.plugin('postcss-px-vw', ({ width = 750, height = 1334, 
         root.walkRules((rule) => {
             rule.walkDecls((decl) => {
                 decl.value = decl.value.replace(/(\d+)px/g, (match, p1) => {
-                    return `${ ((rule.parent.name == 'media' && rule.parent.params.includes('landscape') ? p1 / height : p1 / width) * 100).toFixed(toFixed).replace(/0+$/, '') }vw`
+                    return `${ ((rule.parent.name == 'media' && rule.parent.params.includes('landscape') ? p1 / height : p1 / width) * 100).toFixed(toFixed).replace(/\.?0+$/, '') }vw`
                 })
             })
         })
